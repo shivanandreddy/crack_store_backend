0cf26e88de60993b1b6480c00ab330c8b68e7888
Shivanand <shivanandreddy1@gmail.com>
2026-09-22 21:03:26 +0530

    updated cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT;
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://crackerstorefrontend.vercel.app/"
+  "https://crackerstorefrontend.vercel.app"
 ];
 // Middleware
 app.use(
