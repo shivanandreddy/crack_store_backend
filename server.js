36fecea4b2cc7751deac55a56f118aa3ce9bfb62
Shivanand <shivanandreddy1@gmail.com>
2026-09-22 20:56:56 +0530

    updated cors

--- server.js
+++ server.js
@@ -11,17 +11,30 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://crackerstorefrontend.vercel.app/"
+];
 // Middleware
-app.use(morgan("dev"));
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://crack-store-backend.onrender.com/",
-    ],
+    origin: function (origin, callback) {
+      // Allow requests with no origin (Postman, mobile apps, etc.)
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   })
 );
+app.use(morgan("dev"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
