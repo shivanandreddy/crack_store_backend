1e3750a0350def333510cac005619b2f7bc257e1
Shivanand <shivanandreddy1@gmail.com>
2026-09-22 20:50:45 +0530

    cors configure

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ const app = express();
 const port = process.env.PORT;
 // Middleware
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://crack-store-backend.onrender.com/",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
